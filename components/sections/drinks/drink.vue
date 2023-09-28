bf5992e45044c6abfca265fa79ad0545091d471a
Noxerio <woinyc@seznam.cz>
2023-09-28 16:11:02 +0200

    img

--- components/sections/drinks/drink.vue
+++ components/sections/drinks/drink.vue
@@ -3,6 +3,6 @@
 </script>
 <template>
     <div class="bg-slate-800 rounded-xl transition-all hover:scale-110">
-        <img :src="img" class="rounded-xl transition-all">
+        <img :src="img" class="rounded-xl transition-all" alt="drink swingbar">
     </div>
 </template>
