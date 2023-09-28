fb8c28cd5eb62ffc3c3163d139335d8e292c0842
Noxerio <woinyc@seznam.cz>
2023-09-28 16:10:06 +0200

    hr

--- components/assets/hr.vue
+++ components/assets/hr.vue
@@ -1,3 +1,3 @@
 <template>
-    <img src="/img/hr.png" class="w-40 mx-auto my-10" v-motion-fade-visible>
+    <img src="/img/hr.png" alt="hr swingbar" class="w-40 mx-auto my-10" v-motion-fade-visible>
 </template>
